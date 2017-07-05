e461bb4dc1678e9496ef4886b7277ddecf844c1d
Rob Bagby <rob@robbagby.com>
2017-07-05 12:45:24 -0700

    Updated consumer to calculate a read factor - for better demoability

--- consumer/src/consumer.py
+++ consumer/src/consumer.py
@@ -17,7 +17,7 @@ ADVERTISED_PORT = os.getenv('ADVERTISED_PORT')
 KAFKA_URI = ADVERTISED_HOST + ':' + ADVERTISED_PORT
 print('KAFKA_URI', KAFKA_URI)
 
-CONSUMER_READ_DELAY_IN_SECONDS = float(os.getenv('CONSUMER_READ_DELAY_IN_SECONDS'))
+CONSUMER_READ_DELAY_FACTOR = float(os.getenv('CONSUMER_READ_DELAY_FACTOR'))
 CONSUMER_NUM_MESSAGES_TO_WRITE_LAG = int(os.getenv('CONSUMER_NUM_MESSAGES_TO_WRITE_LAG'))
 CONSUMER_NUM_SECONDS_TO_WRITE_LAG = float(os.getenv('CONSUMER_NUM_SECONDS_TO_WRITE_LAG'))
 
@@ -25,6 +25,8 @@ CASSANDRA_ADDRESS = os.getenv('CASSANDRA_ADDRESS')
 CASSANDRA_PORT = os.getenv('CASSANDRA_PORT')
 CASSANDRA_KEYSPACE = 'sensors'
 
+PUBLISH_NUMBER_OF_SENSORS = int(os.getenv('PUBLISH_NUMBER_OF_SENSORS'))
+PUBLISH_DELAY_IN_SECONDS = int(os.getenv('PUBLISH_DELAY_IN_SECONDS'))
 # session = getConnection('localcassandra', 9042)
 
 
@@ -52,32 +54,28 @@ def consume():
     cassandraclient = CassandraClient()
     session = cassandraclient.getConnection(CASSANDRA_ADDRESS, CASSANDRA_PORT)
     initialize(session)
-    print('I got here')
+
     consumer = KafkaConsumer(group_id='sensortempgroup', bootstrap_servers=KAFKA_URI)
     producer = KafkaProducer(bootstrap_servers=KAFKA_URI)
 
     consumer.subscribe(topics=['sensortemp'])
     last_readtime = datetime.datetime.now()
-    counter = 0
+    lagcounter = 0
+
+    published_items_per_sec = float(PUBLISH_NUMBER_OF_SENSORS) / float(PUBLISH_DELAY_IN_SECONDS)
+    consumed_items_per_sec = published_items_per_sec / float(CONSUMER_READ_DELAY_FACTOR)
+    consumer_delay = 1 / consumed_items_per_sec
+    # waitcounter = 0
 
     for msg in consumer:
         tp = TopicPartition(msg.topic, msg.partition)
         highwater = consumer.highwater(tp)
 
-        
+
         machinetemp = MachineTemperature.from_json(msg.value)
         LOGGER.debug('Inserting record for machineid: ' + machinetemp.machineid)
         cassandraclient.addSensorReading(session, CASSANDRA_KEYSPACE, machinetemp.machineid, machinetemp.eventdate, machinetemp.temperature)
-        # LOGGER.debug("Message Value")
-        # LOGGER.debug(msg.value)
-        # machinetemp = MachineTemperature.from_json(msg.value)
 
-        # LOGGER.debug('Machine id: ' + machinetemp.machineid + '; temperature: ' + str(machinetemp.temperature))
-        
-        # # cassandraclient.addSensorReading(session, CASSANDRA_KEYSPACE, 'sensor1', datetime.datetime.now(), '72F')
-
-        # print("date: " + str(msg) + " partition: " + str(msg.partition)  + "; highwater: " + str(highwater) + " lag: " + str(lag) + "; offset: " + str(msg.offset))
-        
         if highwater is None:
             LOGGER.debug('Highwater was none, resubscribing to topic')
             consumer.unsubscribe()
@@ -85,12 +83,12 @@ def consume():
 
         delta = datetime.datetime.now() - last_readtime
         if (delta.seconds >= CONSUMER_NUM_SECONDS_TO_WRITE_LAG \
-            or counter >= CONSUMER_NUM_MESSAGES_TO_WRITE_LAG) \
+            or lagcounter >= CONSUMER_NUM_MESSAGES_TO_WRITE_LAG) \
             and highwater is not None:
 
             LOGGER.debug("delta.seconds: " + str(delta.seconds) \
                 + "CONSUMER_NUM_SECONDS_TO_WRITE_LAG: " + str(CONSUMER_NUM_SECONDS_TO_WRITE_LAG) \
-                + " counter: " + str(counter) \
+                + " lagcounter: " + str(lagcounter) \
                 + "CONSUMER_NUM_MESSAGES_TO_WRITE_LAG" + str(CONSUMER_NUM_MESSAGES_TO_WRITE_LAG))
 
             lag = (highwater - 1) - msg.offset
@@ -99,12 +97,17 @@ def consume():
                 + ' partition: ' + str(partitionlag.partition))
             producer.send('partitionlag', str.encode(partitionlag.to_json()))
 
-            counter = 0
+            lagcounter = 0
             last_readtime = datetime.datetime.now()
 
-        time.sleep(CONSUMER_READ_DELAY_IN_SECONDS)
-        counter = counter + 1
+        # if waitcounter > PUBLISH_NUMBER_OF_SENSORS:
 
+            # waitcounter = 0
+
+        time.sleep(consumer_delay)
+        
+        lagcounter = lagcounter + 1
+        # waitcounter += 1
 
 if __name__ == "__main__":
     try:
