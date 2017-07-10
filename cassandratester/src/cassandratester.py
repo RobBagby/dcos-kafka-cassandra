86c462fa1e6d02166533263975461f32359b3966
Rob Bagby <rob@robbagby.com>
2017-07-10 13:08:43 -0700

    added a simple cassandra test utility for demo.  Updated DC/OS and VAMP definition files.

---
+++ cassandratester/src/cassandratester.py
@@ -0,0 +1,58 @@
+"""Module for event consumer"""
+import os
+import sys
+import time
+import datetime
+from log import Log
+from cassandraclient import CassandraClient
+import cassandra
+
+CASSANDRA_ADDRESS = os.getenv('CASSANDRA_ADDRESS')
+CASSANDRA_PORT = os.getenv('CASSANDRA_PORT')
+CASSANDRA_KEYSPACE = 'test'
+
+APPLICATION_LOGGING_LEVEL = os.getenv('APPLICATION_LOGGING_LEVEL')
+LOGGER = Log()
+
+def initialize(session):
+    """creates keyspace and creates table"""
+    cassandraclient = CassandraClient()
+    cassandraclient.createKeySpace(session, CASSANDRA_KEYSPACE)
+    cassandraclient.createTemperatureByDayTable(session, CASSANDRA_KEYSPACE)
+
+def readwritetest():
+    """Consumes events from SENSOR_TEMPERATURE_TOPIC topic"""
+    LOGGER.setLevel(APPLICATION_LOGGING_LEVEL)
+    LOGGER.debug('Starting cassandratester')
+    LOGGER.debug('Set Logging Level to ' + APPLICATION_LOGGING_LEVEL)
+    LOGGER.debug('Writing to Cassandra at: ' + CASSANDRA_ADDRESS + ":" + CASSANDRA_PORT)
+    try:
+        cassandraclient = CassandraClient()
+        session = cassandraclient.getConnection(CASSANDRA_ADDRESS, CASSANDRA_PORT)
+        initialize(session)
+
+        while True:
+            cassandraclient.addSensorReading(session, CASSANDRA_KEYSPACE, \
+                'sensor1', datetime.datetime.now(), 72)
+
+            rows = cassandraclient.getLastTenSensorReadings(session, CASSANDRA_KEYSPACE)
+            LOGGER.info('Writing last 10 records')
+            for row in rows:
+                LOGGER.info('time: ' + str(row.event_time) \
+                    + '   sensorid: ' + row.sensor_id + '  temperature' + str(row.temperature))
+            LOGGER.info('\n')
+
+            time.sleep(3)
+    except cassandra.cluster.NoHostAvailable:
+        LOGGER.info('Could not reach Cassandra Server retrying...')
+        time.sleep(3)
+        readwritetest()
+
+if __name__ == "__main__":
+    try:
+        readwritetest()
+    except:
+        e = sys.exc_info()[0]
+        LOGGER.info('exception type: ' + str(type(e)))
+        e = sys.exc_info()[0]
+        LOGGER.error('Unable to consume events', exc_info=True)
